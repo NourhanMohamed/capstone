36753fb44cc20f4a29b5f54fa2d94acf63bfc1f7
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2021-03-07 17:25:50 +0200

    fixed kubernettes script

--- scripts/run-kubernettes.sh
+++ scripts/run-kubernettes.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 dockerpath="mohamn9/capstone"
-kubectl run capstone-pod\
-    --image=$dockerpath\
-    --port=80 --labels app=capstone_pod --image-pull-policy='IfNotPresent'
-kubectl port-forward capstone-pod 8000:80
+kubectl create deployment capstone --image=$dockerpath
+kubectl expose deployment capstone --port=80 --target-port=80 --type=NodePort  --overrides '{ "apiVersion": "v1","spec":{"ports":[{"port":80,"protocol":"TCP","targetPort":80,"nodePort":30031}]}}'
